7b2596cd0f3d0da81a11280d5723284565689e25
lucaspires-source <pires.lucas94@gmail.com>
2021-06-25 14:04:56 -0300

    building header component

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Header() {
+    return (
+        <header className=''>
+            <Image classname="object-contain" src="https://links.papareact.com/ua6"  width={200} height={100}/>
+        </header>
+    )
+}
